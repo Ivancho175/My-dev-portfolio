7e6d192970e351d4d860a5f56cfeb059da08196c
Ivan D. Velez <ivancho175@hotmail.com>
2021-06-06 09:29:44 -0500

    Agrego funcionalidad de cambio de color de acento. Falta cambiar texto descriptivo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
 const darkModeToggle = document.querySelector('#dark-mode-toggle')
+const colorToggle = document.querySelector('#color-toggle')
+const colors = document.querySelectorAll('.color-nav__item')
 let darkMode = localStorage.getItem('darkMode')
 
 navToggle.addEventListener('click', () => {
@@ -24,15 +26,30 @@ const disableDarkMode = () => {
 }
 
 if (darkMode === 'enabled') {
-    enableDarkMode();
+    enableDarkMode()
 }
 
 darkModeToggle.addEventListener('click', () => {
     darkMode = localStorage.getItem('darkMode')
 
     if (darkMode !== 'enabled') {
-        enableDarkMode();
+        enableDarkMode()
     } else {
-        disableDarkMode();
+        disableDarkMode()
     }
 })
+
+colorToggle.addEventListener('click', () => {
+    document.body.classList.toggle('color-nav-open')
+})
+
+let i;
+for (i = 0; i < colors.length; i++) {
+    colors[i].addEventListener('click', changeColor)
+}
+
+function changeColor() {
+    let color = this.getAttribute('data-color')
+    document.documentElement.style.setProperty('--clr-accent', color)
+    document.body.classList.remove('color-nav-open')
+}
