0910c79175c42bc3468c2fdb642e69e366355634
Ivan D. Velez <ivancho175@hotmail.com>
2021-06-06 04:04:59 -0500

    Agrego darkMode. Falta texto descriptivo y switch colores

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
+const darkModeToggle = document.querySelector('#dark-mode-toggle')
+let darkMode = localStorage.getItem('darkMode')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open')
@@ -9,4 +11,28 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open')
     })
+})
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkMode', 'enabled')
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkMode', null)
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode')
+
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 })
