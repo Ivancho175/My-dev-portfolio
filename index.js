27892f405e079116712c567c52d341922ec134a2
Ivan D. Velez <ivancho175@hotmail.com>
2021-06-11 00:43:35 -0500

    Moving things around. Aún falta el texto descriptivo

--- index.js
+++ index.js
@@ -53,3 +53,8 @@ function changeColor() {
     document.documentElement.style.setProperty('--clr-accent', color)
     document.body.classList.remove('color-nav-open')
 }
+
+var ourArray = ["Stimpson", "J", "cat"]
+ourArray.push(["happy", "joy"])
+
+console.log(ourArray)
